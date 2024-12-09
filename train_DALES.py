b4283e9c2debb476858bfc4ba32f7b96d52c995c
d62lu <92398834+d62lu@users.noreply.github.com>
2024-12-09 15:49:05 -0500

    Update train_DALES.py

--- train_DALES.py
+++ train_DALES.py
@@ -146,7 +146,7 @@ def main(args):
                 torch.nn.init.constant_(m.bias.data, 0.0)
 
     try:
-        checkpoint = torch.load(str(experiment_dir) + '/checkpoints/best_model.pth')
+        checkpoint = torch.load(str(experiment_dir) + '/checkpoints/pre_model.pth')
         #start_epoch = checkpoint['epoch']
         start_epoch = 0
         classifier.load_state_dict(checkpoint['model_state_dict'])
